45c305970cc49f2f1514994d317123338690a157
Diego Martinez <diegomrey1@gmail.com>
2024-11-04 19:55:24 +0100

    done exercise

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+const rootStyles = document.documentElement.style;
+const buttonRGBElement = document.getElementById("button-rgb");
+const buttonHexaElement = document.getElementById("button-hexa");
+const titleElement = document.getElementById("title");
+
+const characters = "0123456789ABCDEF";
+
+const getRandomNum = (num) => {
+  return Math.floor(Math.random() * num + 1);
+};
+
+const getColorByRGB = () => {
+  rootStyles.setProperty(
+    "--bg-color",
+    `rgb(${getRandomNum(255)},${getRandomNum(255)},${getRandomNum(255)})`
+  );
+};
+
+const getColorByHexa = () => {
+  let colorHex = "";
+  for (let i = 0; i < 6; i++) {
+    colorHex += characters[getRandomNum(characters.length - 1)];
+  }
+  rootStyles.setProperty("--bg-color", `#${colorHex}`);
+};
+
+const printScroll = () => {
+  let barPorcent = (window.scrollY * 100) / 3197.5;
+  rootStyles.setProperty("--sz-bar", `${barPorcent}%`);
+  titleElement.textContent = `Desplazamiento: ${Math.ceil(barPorcent)}%`;
+};
+
+buttonRGBElement.addEventListener("click", getColorByRGB);
+buttonHexaElement.addEventListener("click", getColorByHexa);
+document.addEventListener("scroll", printScroll);
